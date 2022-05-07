e179b27983a892f4160544fdcbd17870daba8dee
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-05-07 20:44:35 +0200

    feat: add eslint and webpack

---
+++ webpack.config.js
@@ -0,0 +1,123 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const TerserWebpackPlugin = require("terser-webpack-plugin")
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all',
+        }
+    }
+    if (isProd) {
+        config.minimizer = [
+            new CssMinimizerPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+
+    return config
+}
+
+const cssLoaders = (extra) => {
+    const loaders = [MiniCssExtractPlugin.loader, 'css-loader']
+
+    if (extra) {
+        loaders.push(extra)
+    }
+
+    return loaders
+}
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: './js/index.js',
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    
+    optimization: optimization(),
+    devServer: {
+        port: 9000,
+        hot: isDev
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+            inject: true,
+            chunks: ['main'],
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: cssLoaders()
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'static/images/[name][ext]'
+                }
+            },
+            {
+               test: /\.(ttf|woff|woff2|eot)$/,
+               type: 'asset/resource'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'resolve-url-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-transform-runtime']
+                  },
+                }
+              }
+
+        ]
+    }
+
+}
