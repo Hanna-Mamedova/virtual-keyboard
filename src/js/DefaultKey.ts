ea4a9a4ac39f0938861c25cfe01e3693077a5775
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-12-25 13:39:43 +0100

    fix: change DefaultKey to ts

--- src/js/DefaultKey.ts
+++ src/js/DefaultKey.ts
@@ -1,28 +1,31 @@
-import { SHIFT_CHANGE_SYMBOLS, SHIFT_CHANGED_SYMBOLS } from './keyLayoutENG'
+import { SHIFT_CHANGE_SYMBOLS_ENG, SHIFT_CHANGED_SYMBOLS_ENG } from './keyLayoutENG'
 
 export class Key {
-    constructor(value) {
-        this.keyElement = '';
+    keyElement: HTMLButtonElement;
+    value: string;
+    shift: boolean;
+
+    constructor(value: string) {
         this.value = value;
-        this.shift = 'false';
+        this.shift = false;
     }
 
     //Initialize a key
-    init(){
+    init() {
         this.keyElement = document.createElement('button');
         this.keyElement.setAttribute("type", "button");
         this.keyElement.classList.add("keyboard__key");
-        }
-    
+    }
+
     isActive() {
         this.keyElement.classList.add('keyboard__key--active');
-        }
+    }
 
     removeActive() {
         this.keyElement.classList.remove('keyboard__key--active');
-        }
+    }
 
-    createSpecialKey(classes, text, value) {
+    createSpecialKey(classes: string[], text: string, value: string) {
         this.keyElement.classList.add(...classes);
         this.keyElement.innerHTML = text;
         this.value = value;
@@ -30,8 +33,8 @@ export class Key {
 
     onShift() {
         this.shift = !this.shift;
-        if (SHIFT_CHANGE_SYMBOLS.includes(this.value)) {
-            this.keyElement.textContent = SHIFT_CHANGED_SYMBOLS[SHIFT_CHANGE_SYMBOLS.indexOf(this.keyElement.textContent)];
+        if (SHIFT_CHANGE_SYMBOLS_ENG.includes(this.value)) {
+            this.keyElement.textContent = SHIFT_CHANGED_SYMBOLS_ENG[SHIFT_CHANGE_SYMBOLS_ENG.indexOf(this.keyElement.textContent)];
         }
     }
 
